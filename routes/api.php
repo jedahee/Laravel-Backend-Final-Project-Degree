756dba32fe55d0f557bf4a527497d4e1b14db8bf
jedahee <jedahee02@gmail.com>
2022-05-03 16:30:06 +0200

    Organizando codigo

--- routes/api.php
+++ routes/api.php
@@ -28,11 +28,10 @@ use Symfony\Component\HttpFoundation\Response;
 //  NO necesita autenticación
 // ---------------------------
 
-// -- Iniciar sesión --
-Route::post('login', [AuthController::class, 'authenticate']);
 
-// -- Registro --
-Route::post('register', [AuthController::class, 'register']);
+// #####################
+// ## AUTH CONTROLLER ##
+// #####################
 
 // -- Envio de email para recuperar la contraseña --  
 Route::get('forgot-password/{email}', function ($email) {
@@ -64,8 +63,19 @@ Route::get('validation-token/{email}/{get_token}', function ($email, $get_token)
     
 });
 
+// -- Iniciar sesión --
+Route::post('login', [AuthController::class, 'authenticate']);
+
+// -- Registro --
+Route::post('register', [AuthController::class, 'register']);
+
 //  -- Actualización de la contraseña -- 
 Route::put('update-password/{email}/{token}', [AuthController::class, 'updatePassword']);
+
+
+// ######################
+// ## COURT CONTROLLER ##
+// ######################
 
 // -- Obtener todas las pistas --
 Route::get('get-courts', [CourtController::class, 'getCourts']);
@@ -73,78 +83,110 @@ Route::get('get-courts', [CourtController::class, 'getCourts']);
 // -- Obtener una pista por su id --
 Route::get('get-court/{id}', [CourtController::class, 'getCourt']);
 
+// -- Obtener likes y dislikes de una pista por sus comentarios --
+Route::get('get-likes-and-dislikes/{id}', [CourtController::class, 'getLikesAndDislikes']);
+
+
+// ########################
+// ## COMMENT CONTROLLER ##
+// ########################
+
 // -- Obtener comentarios de una pista --
 Route::get('get-comments/{court_id}', [CommentController::class, 'getComments']);
 
-// -- Obtener likes y dislikes de una pista por sus comentarios --
-Route::get('get-likes-and-dislikes/{id}', [CourtController::class, 'getLikesAndDislikes']);
 
 // Necesita autenticación
 // -----------------------
 Route::group(['middleware' => ['jwt.verify']], function() {
-        
-    // -- Obtener todos los usuarios --
-    Route::get('get-users', [AdminController::class, 'getUsers']);
-
+    // #####################
+    // ## AUTH CONTROLLER ##
+    // #####################
+    
     // -- Desconectar sesion --
     Route::post('logout', [AuthController::class, 'logout']);
-    
+
     // -- Obtener usuario --
     Route::post('get-user', [AuthController::class, 'getUser']);
+    
+    
+    // ######################
+    // ## ADMIN CONTROLLER ##
+    // ######################
+    
+    // -- Obtener todos los usuarios --
+    Route::get('get-users', [AdminController::class, 'getUsers']);
 
     // -- Obtener usuario por su id (Admin) --
     Route::post('get-user/{id}', [AdminController::class, 'getUser']);
     
-    // -- Editar nombre de usuario --
-    Route::put('edit-user', [UserController::class, 'editUser']);
+    // -- Añadir advertencia --
+    Route::post('add-warning/{id}', [AdminController::class, 'addWarning']);
 
     // -- Editar nombre de usuario por su id (Admin) --
     Route::put('edit-user/{id}', [AdminController::class, 'editUser']);
-
-    // -- Editar correo del usuario --
-    Route::put('edit-email', [UserController::class, 'editEmail']);
+    
+    // -- Activar o desactivar cuenta de usuario --
+    Route::put('active-desactive-account/{user_id}', [AdminController::class, 'activeDesactiveAccount']);
 
     // -- Editar correo del usuario por su id (Admin) --
     Route::put('edit-email/{id}', [AdminController::class, 'editEmail']);
 
+    // -- Borrar cuenta de usuario por su id (Admin) --
+    Route::delete('delete-account/{id}', [AdminController::class, 'delAccount']);
+
+    
+    // #####################
+    // ## USER CONTROLLER ##
+    // #####################
+
     // -- Obtener advertencias --
     Route::get('get-warnings', [UserController::class, 'getWarnings']);
-
-    // -- Añadir advertencia --
-    Route::post('add-warning/{id}', [AdminController::class, 'addWarning']);
 
     // -- Obtener rol del usuario --
     Route::get('get-role', [UserController::class, 'getRole']);
 
-    // -- Borrar cuenta de usuario --
-    Route::delete('delete-account', [UserController::class, 'delAccount']);
+    // -- Obtener foto de perfil --
+    Route::get('get-image', [UserController::class, 'getImage']);
 
-    // -- Borrar cuenta de usuario por su id (Admin) --
-    Route::delete('delete-account/{id}', [AdminController::class, 'delAccount']);
-    
     // -- Actualizar foto de perfil --
     Route::post('upload-image', [UserController::class, 'uploadImage']);
 
-    // -- Obtener foto de perfil --
-    Route::get('get-image', [UserController::class, 'getImage']);
+    // -- Editar nombre de usuario --
+    Route::put('edit-user', [UserController::class, 'editUser']);
+
+    // -- Editar correo del usuario --
+    Route::put('edit-email', [UserController::class, 'editEmail']);
+
+    // -- Borrar cuenta de usuario --
+    Route::delete('delete-account', [UserController::class, 'delAccount']);
 
     // -- Eliminar foto de perfil --
     Route::delete('delete-image', [UserController::class, 'deleteImage']);
 
+    
+    // ######################
+    // ## COURT CONTROLLER ##
+    // ######################
+
     // -- Añadir pista --
     Route::post('add-court', [CourtController::class, 'addCourt']);
-
-    // -- Eliminar una pista por su id --
-    Route::delete('delete-court/{id}', [CourtController::class, 'deleteCourt']);
-    
-    // -- Obtener todos los usuarios --
-    Route::put('edit-court/{id}', [CourtController::class, 'editCourt']);
 
     // -- Actualizar foto de la pista --
     Route::post('add-image/{id}', [CourtController::class, 'addImage']);
 
     // -- Borrar foto de la pista --
     Route::post('remove-image/{id}', [CourtController::class, 'removeImage']);
+    
+    // -- Obtener todos los usuarios --
+    Route::put('edit-court/{id}', [CourtController::class, 'editCourt']);
+
+    // -- Eliminar una pista por su id --
+    Route::delete('delete-court/{id}', [CourtController::class, 'deleteCourt']);
+    
+    
+    // ########################
+    // ## COMMENT CONTROLLER ##
+    // ########################
 
     // -- Añadir comentario a una pista --
     Route::post('add-comment/{court_id}', [CommentController::class, 'addComment']);
@@ -152,11 +194,26 @@ Route::group(['middleware' => ['jwt.verify']], function() {
     // -- Eliminar comentario de una pista --
     Route::delete('delete-comment/{id}', [CommentController::class, 'deleteComment']);
 
+    
+    // ######################
+    // ## FLOOR CONTROLLER ##
+    // ######################
+
     // -- Obtener tipos de suelo --
     Route::get('get-floors', [FloorController::class, 'getFloors']);
 
+    
+    // ######################
+    // ## SPORT CONTROLLER ##
+    // ######################
+
     // -- Obtener deportes --
     Route::get('get-sports', [SportController::class, 'getSports']);
+
+    
+    // ########################
+    // ## RESERVE CONTROLLER ##
+    // ########################
 
     // -- Obtener reservas --
     Route::get('get-bookings', [ReserveController::class, 'getReserves']);
@@ -170,6 +227,5 @@ Route::group(['middleware' => ['jwt.verify']], function() {
     // -- Eliminar una reserva --
     Route::delete('delete-reserve/{id}', [ReserveController::class, 'deleteReserve']);
 
-    // -- Activar o desactivar cuenta de usuario --
-    Route::put('active-desactive-account/{user_id}', [AdminController::class, 'activeDesactiveAccount']);
+
 });
